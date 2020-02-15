3a23d295a813196e583c142589c1ce3a00cd6a31
dbednar <dbednar13@gmail.com>
2020-02-15 13:20:48 -0600

    Initial

---
+++ FileRenamer/FileRenamer.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace FileRenamer
+{
+    class FileRenamer
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                return;
+            }
+
+            var dicrectories = Directory.EnumerateDirectories(args[0]);
+
+            foreach(var directory in dicrectories)
+            {
+                var nameIndex = directory.LastIndexOf('\\');
+                var name = directory.Substring(nameIndex + 1);
+                var source = Directory.EnumerateFiles(directory).FirstOrDefault();
+                var sourceIndex = source.LastIndexOf('.');
+                var extension = source.Substring(sourceIndex +1);
+                File.Move(source, $"{directory}\\{name}.{extension}");
+            }
+        }
+    }
+}
